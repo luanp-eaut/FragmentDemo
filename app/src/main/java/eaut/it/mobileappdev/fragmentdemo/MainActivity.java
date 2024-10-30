238f2f782618e3dc929d08f0a4ef6b119c91d88b
luanp-eaut <luanp@eaut.edu.vn>
2024-10-30 16:29:22 +0700

    first commit

---
+++ app/src/main/java/eaut/it/mobileappdev/fragmentdemo/MainActivity.java
@@ -0,0 +1,41 @@
+package eaut.it.mobileappdev.fragmentdemo;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.fragment.app.Fragment;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    @Override
+    public void onClick(View view) {
+        int viewId = view.getId();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+
+        Class fragment = viewId == R.id.buttonFragmentA ? FragmentA.class : FragmentB.class;
+        fragmentManager
+                .beginTransaction()
+                .setReorderingAllowed(true)
+                .replace(R.id.fragment_container_view, fragment, null)
+                .commit();
+    }
+}
